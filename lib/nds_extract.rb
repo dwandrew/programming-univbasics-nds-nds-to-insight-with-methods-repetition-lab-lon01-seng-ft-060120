ca1769e891cf3b3dc9a653cb2fec667571db936e
Daniel Andrew <dwandrew@hotmail.com>
2020-02-21 10:19:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -44,6 +44,7 @@ def total_gross(source)
   total= 0;
   while index<source.length
   total += directors_totals(source)["Stephen Spielberg"]
+  puts list_of_directors(source)[index]
   index+= 1
   end
   puts total
